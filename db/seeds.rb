b45db6bcaff93be7697870470c5f8bc288746663
Rudi Thiel <rudithiel16@gmail.com>
2017-11-13 21:41:36 +0000

    What am I doing. Its 00:00. Go sleep

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,22 +3,3 @@
 #
 # Examples:
 #
-Subject.delete_all
-
-afr = Subject.create(name: "Afrikaans Huistaal", papers: 3, code: "AFHT");
-afr.save
-
-it = Subject.create(name: "Information Technology", papers: 2, code: "INFT");
-it.save
-
-bio = Subject.create(name: "Life Sciences", papers: 2, code: "LFSC")
-bio.save
-
-maths = Subject.create(name: "Mathematics", papers: 2, code: "MATH");
-maths.save;
-
-science = Subject.create(name: "Physical Sciences", papers: 2, code: "PHSC");
-science.save
-
-User.create(email: "rudi@rapstudios.co.za", admin: "true", password: "Rudi1Karl2")
-User.create(email: "philip@rapstudios.co.za", admin: "true", password: "Awewert1")
