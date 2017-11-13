907b46cadef878a1a4fb15599ecff9983128ce25
Rudi Thiel <rudithiel16@gmail.com>
2017-11-13 12:22:08 +0000

    Fix local image display

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,27 +1,9 @@
 class Category < ActiveRecord::Base
-  paperclip_opts = {
-  :styles => { :medium => '150x200>' },
-  :convert_options => { :all => '-quality 92' },
-  :processor       => [ :cropper ]
-  }
-  
-  unless Rails.env.development?
-    paperclip_opts = { :storage => :s3,
-                          :url => ":s3_domain_url",
-                          :path => ':attachment/:id/:style.:extension',
-                          :s3_region => ENV['AWS_REGION'], 
-                          :s3_credentials => {
-                            :bucket => ENV['AWS_BUCKET_NAME'],
-                            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-                          },
-                          :bucket         => ENV['AWS_BUCKET_NAME'] }
-  end
-  
+
   belongs_to :subject
   has_many :questions
   attr_accessor :subject_name
-  has_attached_file :avatar, paperclip_opts
+  has_attached_file :avatar
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   validates :subject_id, presence: true, numericality: true
   validates :name, presence: true, uniqueness: true
