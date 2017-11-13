84b9116e9ca90d5be92cdadc320e077cc57d072c
Rudi Thiel <rudithiel16@gmail.com>
2017-11-13 09:48:34 +0000

    add url to s3 options

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,19 +1,15 @@
 class Category < ActiveRecord::Base
-  paperclip_opts = {
-  :styles => { :thumb => '40x40#', :medium => '150x200>', :large => '300x300>' },
-  :convert_options => { :all => '-quality 92' },
-  :processor       => [ :cropper ]
-  }
   
   unless Rails.env.development?
-    paperclip_opts.merge! :storage        => :s3,
+    paperclip_opts = { :storage        => :s3,
+                          :url => ":s3_domain_url",
                           :s3_region => ENV['AWS_REGION'], 
                           :s3_credentials => {
                             :bucket => ENV['AWS_BUCKET_NAME'],
                             :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
                             :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
                           },
-                          :bucket         => ENV['AWS_BUCKET_NAME']
+                          :bucket         => ENV['AWS_BUCKET_NAME'] }
   end
   
   belongs_to :subject
